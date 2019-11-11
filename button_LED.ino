03d4469c2d734c8e0a1921e7b4ce2f23e1b36ee1
Tea Panelli <55266921+teapanelli@users.noreply.github.com>
2019-11-11 11:01:01 -0800

    button turns LED on/off

---
+++ button_LED.ino
@@ -0,0 +1,22 @@
+//read button input
+int ledPin = 13;
+int buttonPin = 8;
+
+void setup() {
+  Serial.begin(19200);
+  pinMode (ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+
+
+}
+
+void loop() {
+  if (digitalRead(buttonPin) == LOW) {
+    digitalWrite (ledPin, HIGH);
+    Serial.println("Turning LED on");
+  }
+
+  else {
+    digitalWrite (ledPin, LOW);
+  }
+}
